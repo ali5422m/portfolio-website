941f755dd477e66291fd64e694690fd5ca3cc8ef
Ali <alimohammadinia3141@gmail.com>
2023-10-07 12:37:11 +0330

    complete work

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,7 +1,100 @@
 import React from 'react';
+import {motion} from "framer-motion";
+import {fadeIn} from "../variants";
+
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
-  return <div className="section" id="work">Work</div>;
+    return (
+        <section className="section" id="work">
+            <div className="container mx-auto">
+                <div className="flex flex-col lg:flex-row gap-x-10">
+                    <motion.div
+                        variants={fadeIn("right", 0.3)}
+                        initial="hidden"
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
+                        <div>
+                            <h2 className="h2 leading-tight text-accent">
+                                My Latest <br/>
+                                work.
+                            </h2>
+                            <p className="max-w-sm mb-16">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                                Aliquid asperiores corporis dignissimos,
+                                doloribus ducimus est excepturi expedita illo nam obcaecati pariatur porro praesentium
+                                quas quidem quo,
+                                quos sit totam voluptatum.
+                            </p>
+                            <button className="btn btn-sm">View all projects</button>
+                        </div>
+                        <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                            <div
+                                className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                            <img
+                                src={Img1}
+                                alt=""
+                                className="group-hover:scale-125 transition-all duration-500"
+                            />
+                            <div className="absolute -bottom-full left-12 group-hover:bottom-24
+                              transition-all duration-500 z-50">
+                                <span className="text-gradient"> UI/UX Design</span>
+                            </div>
+                            <div
+                                className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                                <span className="text-3xl text-white">Project Title</span>
+                            </div>
+                        </div>
+
+                    </motion.div>
+                    <motion.div
+                        variants={fadeIn("left", 0.2)}
+                        initial="hidden"
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.3 }}
+                        className="flex-1 flex flex-col gap-y-10 justify-end">
+                        <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                            <div
+                                className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                            <img
+                                src={Img2}
+                                alt=""
+                                className="group-hover:scale-125 transition-all duration-500"
+                            />
+                            <div className="absolute -bottom-full left-12 group-hover:bottom-24
+                              transition-all duration-500 z-50">
+                                <span className="text-gradient"> UI/UX Design</span>
+                            </div>
+                            <div
+                                className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                                <span className="text-3xl text-white">Project Title</span>
+                            </div>
+                        </div>
+                        <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+                            <div
+                                className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+                            <img
+                                src={Img3}
+                                alt=""
+                                className="group-hover:scale-125 transition-all duration-500"
+                            />
+                            <div className="absolute -bottom-full left-12 group-hover:bottom-24
+                              transition-all duration-500 z-50">
+                                <span className="text-gradient"> UI/UX Design</span>
+                            </div>
+                            <div
+                                className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                                <span className="text-3xl text-white">Project Title</span>
+                            </div>
+                        </div>
+                    </motion.div>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Work;
